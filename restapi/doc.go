f070439da9994edf3f81e74532f74a2b8c47a5f9
nathking <nathking@uk.ibm.com>
2017-04-03 21:30:30 +0100

    Add first code

---
+++ restapi/doc.go
@@ -0,0 +1,19 @@
+/*Package restapi Temperature
+
+    Schemes:
+      http
+    Host: $(catalog.host)
+    BasePath: /temperature
+    Version: 1.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
